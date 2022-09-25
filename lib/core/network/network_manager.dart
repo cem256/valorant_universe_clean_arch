d9dd0b6782398e8662f0868b137bb124b30ed296
cem256 <cemo99@gmail.com>
2022-09-25 12:22:03 +0300

    Cleanup

--- lib/core/network/network_manager.dart
+++ lib/core/network/network_manager.dart
@@ -4,21 +4,6 @@ import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 import '../constants/strings.dart';
 
-// class NetworkManager {
-//   static NetworkManager? _instance;
-//   static NetworkManager get instance {
-//     _instance ??= NetworkManager._init();
-//     return _instance!;
-//   }
-
-//   NetworkManager._init() {
-//     dio = Dio(BaseOptions(baseUrl: Strings.valorantApiBaseUrl));
-//     if (kDebugMode) dio.interceptors.add(PrettyDioLogger(responseBody: false));
-//   }
-
-//   late final Dio dio;
-// }
-
 class NetworkManager {
   late final Dio dio;
 
